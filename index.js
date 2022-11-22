c5c52cfa9e0efc006f40b8ab1322c7142e2f4fb0
Wesley-waka <wesley.waka@student.moringaschool.com>
2022-11-22 14:40:57 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+let companyName = 'Scuber';
+// const companyName = "Expected companyName to be a const";
+
+let mostProfitableNeighborhood = 'Chelsea';
+// let mostProfitableNeighborhood = "Expected mostProfitableNeighborhood to be defined using let";
+
+let companyCeo = `Susan Smith`;
+// let companyCeo = "Expected companyCeo to be defined using let";
+
+
+
 // Code your solution in this file!
